7b4b814aa0c7f877df6714ed46d9ebc30a3b403d
Parul-Byte <parul1567.becse24@chitkara.edu.in>
2025-02-16 23:22:58 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -7,8 +7,7 @@ int main() {
     
     if (isdigit(ch))
         printf("Digit\n");
-    else if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 
-             ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+    else if  (strchr("aeiouAEIOU", ch))
         printf("Vowel\n");
     else if (isalpha(ch))
         printf("Consonant\n");
@@ -17,3 +16,7 @@ int main() {
     
     return 0;
 }
+    
+
+
+
