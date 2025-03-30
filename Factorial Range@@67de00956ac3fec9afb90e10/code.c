72aa453e5b3b538025df3e1a6439cd05e7a5eb22
Parul Sharma <jenniekim11198@gmail.com>
2025-03-30 22:10:20 +0530

    Solution to Factorial Range

--- Factorial Range@@67de00956ac3fec9afb90e10/code.c
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
-int factorial(int n){
-    if (n>0){
-        return n*factorial(n-1);
-            }else{
-                return 1;
-            }
-            }
+// Recursive function to calculate factorial
+int factorial(int n) {
+    if (n > 0) {  // Base condition check
+        return n * factorial(n - 1);  // Recursive call
+    } else {
+        return 1;  // Base case: factorial(0) = 1
+    }
+}
 
-            int main(){
-                int num;
-                scanf("%d", &num);
-                if (num<0){
-                    printf("Factorial is not defined\n");
-                } else{
-                    printf("Factorial of %d is %d\n", num, factorial(num));
-                }
-            
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\\n");
+    } else {
+        printf("Factorial of %d is %d\\n", num, factorial(num));
+    }
 
     return 0;
 }
