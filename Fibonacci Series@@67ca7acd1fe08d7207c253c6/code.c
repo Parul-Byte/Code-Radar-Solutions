353ffc03427dbf12199c0e24d73f2e5be3cfd93a
Parul Sharma <jenniekim11198@gmail.com>
2025-03-30 13:57:07 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n, a=0, b=1, c;
+    printf()
+    scanf("%d", &n);
+    while (n--){
+    printf("%d",a);
+    c=a+b;
+    a=b;
+    b=c;}
+
+    return 0;
+}
